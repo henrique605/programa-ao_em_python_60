9f0568ce8ae6e7e6cb3485608b3e49d63f4ac99e
henrique605 <hen.oliveira2026@gmail.com>
2026-02-26 17:24:11 -0300

    Add files via upload

---
+++ linguagem Python/aula 2.py
@@ -0,0 +1,8 @@
+# instamos a linguagem de programaçao
+
+# verificar se a instalaçao foi realizada corretamente
+
+# testamos a linguagem
+
+print('hello Mundo....')
+print('teste')
---
+++ linguagem Python/dados primitivos.py
@@ -0,0 +1,20 @@
+# dados primitivos em python
+
+# tipo string - texto s
+'texto'
+"texto"
+
+#tipo interger - inteiro
+print(999+2)
+1
+2
+100
+
+#tipo float - real
+0.5
+2.10
+100.
+
+# tipo bool - logico
+true
+false
