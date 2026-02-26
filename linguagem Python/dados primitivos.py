150f230be8ec97f09e3e4c98b19423cbe30c2b39
henrique605 <hen.oliveira2026@gmail.com>
2026-02-26 17:52:45 -0300

    Update dados primitivos.py

--- linguagem Python/dados primitivos.py
+++ linguagem Python/dados primitivos.py
@@ -16,5 +16,16 @@ print(999+2)
 100.
 
 # tipo bool - logico
-true
-false
+True
+False
+
+# contatenaçao = juntar dados 
+
+print('olá Mundo', 'Henrique')
+print('Resultado da soma: ', 2+2)
+print('resultado da divisão; ',2/2)
+print(' o aluno passou de ano?', True)
+print('altura: ', 1.72)
+print('R$', 500.0)
+print('teste', 2 ,5.2, False)
+print(1236 , 'teste')
