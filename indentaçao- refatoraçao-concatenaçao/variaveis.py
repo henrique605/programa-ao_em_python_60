a8fe8ad63d2c976dd0a8439ec101b153bd32b413
henrique605 <hen.oliveira2026@gmail.com>
2026-03-05 15:49:26 -0300

    Add files via upload

---
+++ indentaçao- refatoraçao-concatenaçao/variaveis.py
@@ -0,0 +1,65 @@
+#Multiplique 4 por 6 e divida o resultado por 2. (Concatene com a palavra resultado)
+
+print('Resultado:',int((4*6)/2))
+
+# Calcule a soma de 5 e 7.(Concatene com a palavra resultado)
+
+print('resultado:', int(5+7))
+
+#Eleve 3 à potência de 4.(Concatene com a palavra resultado)
+
+print('resultado:', int(3**4))
+
+#Calcule a média de 8, 12 e 15.(Concatene com a palavra resultado)
+
+print('resultado:', round(((8+12+15)/3),2))
+
+#Subtraia 10 de 2 e multiplique o resultado por 5.(Concatene com a palavra resultado)
+
+print('resultado:', int((10-2)*5))
+
+#Divida 27 por 3 e some 5.(Concatene com a palavra resultado)
+
+print('resultadp:', int(27/3+5))
+
+#OPCIONAL******************
+
+#Calcule o módulo ( % ) de 17 por 4.
+
+print(14%4)
+
+#Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+
+print((9*3)**2)
+
+#Calcule a raiz quadrada de 81.
+
+print(int(81**0.5))
+
+#Adicione 20 a 3 vezes 4.
+
+print(20+3*4)
+
+#Multiplique 15 por 2 e, em seguida, subtraia 7.
+
+print(15*2-7)
+
+#Eleve 5 ao cubo.
+
+print(5**3)
+
+#Calcule a média de 17, 21 e 25.
+
+print(int((17+21+25)/3))
+
+#Multiplique 11 por 2 e adicione 7.
+
+print(11*2+7)
+
+#Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+
+print((3*8-15)/2)
+
+#Eleve 2 à potência de 10.
+
+print(2**10)
